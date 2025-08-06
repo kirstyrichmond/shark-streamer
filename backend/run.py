95f0906e6237d3e2909072722c15d509ce703db0
Kirsty Richmond <kirsty@madebysauce.com>
2025-08-07 00:39:57 +0100

    updated run.py

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,8 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        print("✅ Database created successfully!")
-    app.run(debug=True, port=5001)
+        port = int(os.environ.get('PORT', 5001))
+        app.run(host='0.0.0.0', port=port, debug=False)
