71db48ca67cf2664b8f500044464abac25bf83b0
kirstyrichmondMBS <kirsty@madebysauce.com>
2025-09-13 18:20:17 +0100

    Optimised MovieCard and moved reusable movie details logic

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,92 +4,46 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import {
   Container,
   Image,
-  NoImage,
-  NoImageText,
-  // Info,
-  // TitleContainer,
-  // Title,
-  // MediaType,
-  // Overview,
-  // RatingContainer,
-  // Rating,
-  // ReleaseDate,
 } from "../styles/MovieCard.styles";
+import { getImageUrl, getMovieTitle } from "../utils/movieUtils";
 
 const MovieCard = ({ movie, onClick, isSelected }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
-  const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   const handleClick = () => {
-    if (onClick) {
-      onClick(movie);
-    }
+    onClick?.(movie);
   };
-  
-  const isMovie = movie.media_type === 'movie' || movie.title;
-  const isTV = movie.media_type === 'tv' || movie.name;
-  
-  const getTitle = () => {
-    if (isMovie) return movie.title;
-    if (isTV) return movie.name;
-    return movie.original_name || 'Unknown Title';
-  };
-  
-  // const getYear = () => {
-  //   if (isMovie && movie.release_date) return movie.release_date.substring(0, 4);
-  //   if (isTV && movie.first_air_date) return movie.first_air_date.substring(0, 4);
-  //   return 'N/A';
-  // };
+
+  const imageSrc = getImageUrl(movie.poster_path || movie.backdrop_path, 'w500');
 
   return (
     <Container
       onClick={handleClick}
       $isSelected={isSelected}
     >
-      {movie.backdrop_path || movie.poster_path ? (
-        <>
-          {!imageLoaded && (
-            <Skeleton 
-              height="100%"
-              baseColor="#202020"
-              highlightColor="#444"
-            />
-          )}
-          <Image
-            src={`${BASE_URL}${movie.poster_path || movie.backdrop_path}`}
-            alt={getTitle()}
-            onLoad={() => setImageLoaded(true)}
-            style={{ display: imageLoaded ? 'block' : 'none' }}
-          />
-        </>
-      ) : (
-        <NoImage>
-          <NoImageText>No Image Available</NoImageText>
-        </NoImage>
+      {!imageLoaded && (
+        <Skeleton 
+          height="100%"
+          baseColor="#202020"
+          highlightColor="#444"
+        />
       )}
-      {/* <Info isHovered={isHovered}> */}
-        {/* <TitleContainer>
-          <Title>{getTitle()}</Title>
-          <MediaType>{isMovie ? 'Movie' : 'TV'}</MediaType>
-        </TitleContainer> */}
-        
-        {/* {isHovered && (
-          <Overview>
-            {movie.overview?.length > 150
-              ? `${movie.overview.substring(0, 150)}...`
-              : movie.overview || 'No description available'}
-          </Overview>
-        )}
-        {isHovered && (
-          <RatingContainer>
-            <Rating>{movie.vote_average?.toFixed(1)}/10</Rating>
-            <ReleaseDate>{getYear()}</ReleaseDate>
-          </RatingContainer>
-        )} */}
-      {/* </Info> */}
+      <Image
+        src={imageSrc}
+        alt={getMovieTitle(movie)}
+        onLoad={() => setImageLoaded(true)}
+        loading="lazy"
+        style={{ opacity: imageLoaded ? 1 : 0 }}
+      />
     </Container>
   );
 };
 
 
-export default MovieCard;
+export default React.memo(MovieCard, (prevProps, nextProps) => {
+  return (
+    prevProps.movie.id === nextProps.movie.id &&
+    prevProps.isSelected === nextProps.isSelected &&
+    prevProps.onClick === nextProps.onClick
+  );
+});
